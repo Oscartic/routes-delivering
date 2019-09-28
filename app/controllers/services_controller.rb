c2341610b5d61c8d1acf559f93539445a37f875c
OscarB <oballesteros@abstract.cl>
2019-09-28 17:48:21 -0300

    implement bootstrap solved, _form partial service and show view include

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,11 +1,27 @@
 class ServicesController < ApplicationController
+  before_action :set_service, only: [:show, :edit, :update, :destroy]
   def index
   end
 
+  def show
+  end
+
   def new
+    @service = Service.new
   end
 
   def create
+    binding.pry
+    @service = Service.new(service_params)
+    respond_to do |format|
+      if @service.save
+        format.html { redirect_to @service, notice: 'Category was successfully created.' }
+        format.json { render :show, status: :created, location: @service }
+      else
+        format.html { render :new }
+        format.json { render json: @service.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def edit
@@ -16,4 +32,14 @@ class ServicesController < ApplicationController
 
   def destroy
   end
+
+  private
+  def set_service
+    @service = Service.find(params[:id])
+  end
+
+  def service_params
+    params.require(:service).permit(:type_service, :address, :weight, :commune_id, :route_id, :load_id)
+  end
+
 end
