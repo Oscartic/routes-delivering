449fc2b751e4550c3fe41e67f5969d05d0870e3a
OscarB <oballesteros@abstract.cl>
2019-09-28 21:26:49 -0300

    Driver controller, views create / new / index added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :services
   resources :vehicles
+  resources :drivers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
