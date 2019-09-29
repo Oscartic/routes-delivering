df907198f355a68944b1047843cd98fce4477a7f
OscarB <oballesteros@abstract.cl>
2019-09-29 14:46:54 -0300

    vehicle and driver assignment (null case)

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -5,6 +5,10 @@ class RoutesController < ApplicationController
 
   def new
     @route = Route.new
+    no_route = Service.where(route_id: nil).map{ |service| service.commune_id }
+    @Communes = Commune.where(id: no_route)
+    @services_without_route = Service.where(route_id: nil)
+
   end
 
   def show
@@ -12,13 +16,19 @@ class RoutesController < ApplicationController
 
   def create
     @route = Route.new(route_params)
+    @route.int_array = params[:route][:int_array]
     binding.pry
+    driver = Driver.where(vehicle_id: nil).first
+    vehicle = Vehicle.where(driver_id: nil, load_id: @route.load).first
+    @route.driver = driver
+    @route.vehicle = vehicle
+    params[:route][:services_array]
+    params[:route][:load_id]
     #@route.starts_at = params[:route][:starts_at].strftime("%Y-%m-%d %H:%M:%S")
     #@route.ends_at = params[:route][:ends_at].strftime("%Y-%m-%d %H:%M:%S")
-    @route = Route.new(route_params)
     respond_to do |format|
       if @route.save
-        format.html { redirect_to @route, notice: 'Category was successfully created.' }
+        format.html { redirect_to routes_path, notice: 'Route was successfully created.' }
         format.json { render :show, status: :created, location: @route }
       else
         format.html { render :new }
@@ -42,7 +52,15 @@ class RoutesController < ApplicationController
   end
 
   def route_params
-    params.require(:route).permit(:starts_at, :ends_at, :load_sum, :load_id, :int_array, :stops_amount, :is_completed)
+    params.require(:route).permit(:starts_at,
+                                  :ends_at,
+                                  :load_sum,
+                                  :load_id,
+                                  :int_array,
+                                  :services_array,
+                                  :stops_amount,
+                                  :is_completed
+                                  )
   end
 
 end
