c60322bac48bf268cf211b93ba04be9be5cbd8c2
OscarB <oballesteros@abstract.cl>
2019-09-28 22:44:53 -0300

    Routes views and navbar to added

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :services
   resources :vehicles
   resources :drivers
+  resources :routes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
